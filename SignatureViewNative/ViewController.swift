0f0a125733e14c8c65bb496c0adc35a35f5f84c1
SamiBeig <Sami.Beig45@myhunter.cuny.edu>
2020-03-25 15:21:37 -0400

    able to sign in box only

--- SignatureViewNative/ViewController.swift
+++ SignatureViewNative/ViewController.swift
@@ -10,67 +10,77 @@ import UIKit
 
 
 class Canvas: UIView {
+  
+  
+  func clear(){
+    lines.removeAll()
+    setNeedsDisplay()
+  }
+  
+  override func draw(_ rect: CGRect) {
+    super.draw(rect)
     
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
-        
-        guard let context = UIGraphicsGetCurrentContext() else { return }
-
-        context.setStrokeColor(UIColor.red.cgColor)
-        context.setLineWidth(10)
-        context.setLineCap(.butt)
-        
-        lines.forEach { (line) in
-            for (i, p) in line.enumerated() {
-                if i == 0 {
-                    context.move(to: p)
-                } else {
-                    context.addLine(to: p)
-                }
-            }
+    guard let context = UIGraphicsGetCurrentContext() else { return }
+    
+    context.setStrokeColor(UIColor.black.cgColor)
+    context.setLineWidth(10)
+    context.setLineCap(.butt)
+    
+    lines.forEach { (line) in
+      for (i, p) in line.enumerated() {
+        if i == 0 {
+          context.move(to: p)
+        } else {
+          context.addLine(to: p)
         }
-        
-        context.strokePath()
-        
-    }
-
-    var lines = [[CGPoint]]()
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        lines.append([CGPoint]())
+      }
     }
     
-    // track the finger as we move across screen
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let point = touches.first?.location(in: nil) else { return }
-        
-        guard var lastLine = lines.popLast() else { return }
-        lastLine.append(point)
-        lines.append(lastLine)
-        setNeedsDisplay()
-    }
+    context.strokePath()
     
+  }
+  
+  var lines = [[CGPoint]]()
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    lines.append([CGPoint]())
+  }
+  
+  // track the finger as we move across screen
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let point = touches.first?.location(in: self) else { return }
+    
+    guard var lastLine = lines.popLast() else { return }
+    lastLine.append(point)
+    lines.append(lastLine)
+    setNeedsDisplay()
+  }
+  
 }
- 
+
 
 
 class ViewController: UIViewController {
-    
+  
   
   @IBOutlet weak var signatureView: UIView!
   
-  let canvas = Canvas()
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      
-      
-      view.addSubview(canvas)
-      canvas.backgroundColor = .white
-      canvas.frame = view.frame
-      
-      
-    }
+  let canvas = Canvas()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+    view.addSubview(canvas)
+    canvas.backgroundColor = .gray
+    canvas.frame = signatureView.frame
+    
+  }
+  
+  @IBAction func clearButton(_ sender: Any) {
+    canvas.clear()
+  }
 
 }
 

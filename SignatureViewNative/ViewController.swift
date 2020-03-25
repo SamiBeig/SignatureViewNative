276123fd5df4dc1b86e970f840a4cc2c6778dcdb
SamiBeig <Sami.Beig45@myhunter.cuny.edu>
2020-03-25 12:50:52 -0400

    Initial Commit

---
+++ SignatureViewNative/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignatureViewNative
+//
+//  Created by Sami Beig on 3/25/20.
+//  Copyright © 2020 Sami Beig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
